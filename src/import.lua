56469b1c26e5534ebccc78d5ba8c55dc5bbf242e
Chris Ess <caess@ithiriel.com>
2009-12-06 15:22:19 -0500

    Fixed local functions in import.lua to conform to 'best practices' coding standard.

--- src/import.lua
+++ src/import.lua
@@ -1,4 +1,4 @@
-local function Greg_Parse_Points( name, points_string )
+local function Greg_ParsePoints( name, points_string )
   local points_iteration_pattern = '%w+: %-?%d+%.%d+'
   local points_split_pattern     = '(%w+): (%-?%d+%.%d+)'
   
@@ -9,7 +9,7 @@ local function Greg_Parse_Points( name, points_string )
   end
 end
 
-local function Greg_Parse_Alts( name, alts_string )
+local function Greg_ParseAlts( name, alts_string )
   local alt_pattern = '%u%a+'
   
   for alt in string.gmatch( alts_string, alt_pattern ) do
@@ -17,14 +17,14 @@ local function Greg_Parse_Alts( name, alts_string )
   end
 end
 
-local function Greg_Parse_Line( line )
+local function Greg_ParseLine( line )
   local line_pattern = '^%s*(%u%a+) | Points: ([^|]+) | Alts: ?([%a, ]*)$'
   local name, points, alts = string.match( line, line_pattern )
 
   Greg_Standings[ name ] = {}
     
-  Greg_Parse_Points( name, points )
-  Greg_Parse_Alts( name, alts )
+  Greg_ParsePoints( name, points )
+  Greg_ParseAlts( name, alts )
 end
 
 function Greg_Import( lines )
@@ -33,7 +33,7 @@ function Greg_Import( lines )
   
   for line in string.gmatch( lines, "[^\n]+") do
     if line ~= '' then
-      Greg_Parse_Line( line )
+      Greg_ParseLine( line )
     end
   end
 end
